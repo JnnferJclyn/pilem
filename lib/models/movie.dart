34164c346f296e108a43b81d17ea94a99a87374f
JnnferJclyn <132757236+JnnferJclyn@users.noreply.github.com>
2024-02-27 11:12:49 +0700

    pertemuan3
    philem

---
+++ lib/models/movie.dart
@@ -0,0 +1,32 @@
+import 'dart:js_interop';
+
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterpath;
+  final String backdropPath;
+  final String releaseDate;
+  final double voteAverage;
+
+  Movie(
+      {required this.id,
+      required this.title,
+      required this.overview,
+      required this.posterpath,
+      required this.backdropPath,
+      required this.releaseDate,
+      required this.voteAverage});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterpath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      releaseDate: json['release_date'],
+      voteAverage: double.parse(json['vote_average'])
+    );
+  }
+}
